631b5e84ec5c2a2584c7d0eb97a258de5f0e1450
AnMikhaylova <mih.anast@rambler.ru>
2023-11-29 16:21:19 +0300

    изменены названия функций. добавлена нормализация фреймов и метрика для кгр и дыхания

--- nirv2.py
+++ nirv2.py
@@ -9,31 +9,38 @@ import re
 import os
 from icecream import ic
 from scipy.stats import zscore
+import matplotlib.pyplot as plt
+
+# Окна начала реакции
+EDA_WINDOW = {'start': 1200, 'end': 8000}  # КГР
+PLE_WINDOW = {'start': 2000, 'end': 9000}  # ФПГ
+BREATHE_WINDOW = {'start': 1000, 'end': 90000}  # Дыхание
+BLOOD_VOL_WINDOW = {'start': 0, 'end': 10000}  # Давление
 
 
 # для изменения каналов полиграфа надо изменить значение списка polyChannels
-def argParser():
+def arg_parser():
     parser = argparse.ArgumentParser(description='Script polygraph')
     parser.add_argument("--dir", type=str, required=True, help="This is the path to the root data folder")
     # parser.add_argument("--sub", type=str, required=True, help="This is the subject's name")
     args = parser.parse_args()
-    dataDir = args.dir + "\\"
-    faceData = dataDir + 'facereader\\'
-    polyData = dataDir + 'polygraph\\'
-    timeData = dataDir + 'time\\'
-    polyChannels = ["ABDOMINAL_RESP", "ABS_BLOOD_VOLUME", "BLOOD_VOLUME", "EDA", "HEART_RATE", "PLE", "THORACIC_RESP",
-                    "TONIC_EDA", "TREMOR"]
-    # polyChannels = ["ABDOMINAL_RESP", "BLOOD_VOLUME", "EDA", "PLE", "THORACIC_RESP"]
+    data_dir = args.dir + "\\"
+    face_data = data_dir + 'facereader\\'
+    poly_data = data_dir + 'polygraph\\'
+    time_data = data_dir + 'time\\'
+    # poly_channels = ["ABDOMINAL_RESP", "ABS_BLOOD_VOLUME", "BLOOD_VOLUME", "EDA", "HEART_RATE", "PLE", "THORACIC_RESP",
+    #                 "TONIC_EDA", "TREMOR"]
+    poly_channels = ["ABDOMINAL_RESP", "BLOOD_VOLUME", "EDA", "PLE", "THORACIC_RESP"]
 
-    file_names = os.listdir(faceData)
+    file_names = os.listdir(face_data)
     subjects = list(map(lambda x: x.replace('.txt', ''), file_names))  # имена всех участников (субъектов) эксперимента
-    metaInf = {'dataDir': dataDir, 'faceData': faceData, 'polyData': polyData, 'timeData': timeData,
-               'polyChannels': polyChannels, 'subjects': subjects}
-    return metaInf
+    meta_inf = {'data_dir': data_dir, 'face_data': face_data, 'poly_data': poly_data, 'time_data': time_data,
+                'poly_channels': poly_channels, 'subjects': subjects}
+    return meta_inf
 
 
 # #### Функция для конвертации объекта datetime.time в миллисекунды
-def totalMiliSec(time):
+def total_mil_sec(time):
     return (
             time.microsecond + time.second * 1000000 + time.minute * 60 * 1000000 +
             time.hour * 60 * 60 * 1000000) / 1000
@@ -47,18 +54,18 @@ def filter_stim(s):
 # #### Функция для разметки стимулов (выделение из таблицы временной разметки для конкретного исследуемого)
 # Выходное значение: словарь с временной разметкой по стимулам для конкретного исследуемого,
 # время представлено в миллисекундах
-def stimPrep(df, sID):  # df = timeline, sID - исследуемый
-    stimulRead = filter(filter_stim,
-                        list(df.columns[2:]))  # фильтруем список стимулов из датафрейма, чтоб убрать нулевые
+def stim_prep(df, sID):  # df = timeline, sID - исследуемый
+    stimul_read = filter(filter_stim,
+                         list(df.columns[2:]))  # фильтруем список стимулов из датафрейма, чтоб убрать нулевые
     # получаем список нужных стимулов
-    stimMilisec = {}
-    for s in stimulRead:
-        stimMilisec[s] = totalMiliSec(df.loc[df['id'] == sID][s].iloc[0].time())
-    return stimMilisec
+    stim_millisecond = {}
+    for s in stimul_read:
+        stim_millisecond[s] = total_mil_sec(df.loc[df['id'] == sID][s].iloc[0].time())
+    return stim_millisecond
 
 
 # #### Функция для сопоставления времени в файлах со временем предъявления стимулов
-def stimAndTime(timeline, t):  # timeline = timeline in milisec (dictionary), t - time
+def stim_and_time(timeline, t):  # timeline = timeline in millisecond (dictionary), t - time
     dic = {}
     # dic = {key: val for key, val in df.items() if ((val <= t) & (t <= val + 10000))}
     for i, (key, value) in enumerate(timeline.items()):
@@ -78,7 +85,7 @@ def stimAndTime(timeline, t):  # timeline = timeline in milisec (dictionary), t 
 
 
 # #### Функция для копирования и конвертации фреймов полиграфа
-def copyAconvert(df):
+def copy_a_convert(df):
     df1 = df.copy(deep=True)
     df1['Time'] = df1['Time'].map(lambda x: (x.microsecond + x.second * 1000000 + x.minute * 60 * 1000000 +
                                              x.hour * 60 * 60 * 1000000) / 1000)
@@ -86,7 +93,7 @@ def copyAconvert(df):
 
 
 # #### Функция для усреднения строк файлов полиграфа
-def sortPoly(df, t):
+def sort_poly(df, t):
     rows = df[(t <= df['Time']) & (df['Time'] < t + 50)]
     if len(rows) > 1:
         tot = rows['Value'].mean()
@@ -98,7 +105,7 @@ def sortPoly(df, t):
 
 
 # #### Функция для усреднения строк файлов FaceReader
-def sortEmot(df, t):
+def sort_emot(df, t):
     rows = df[(t <= df['Video Time']) & (df['Video Time'] < t + 50)]
     if len(rows) > 1:
         tot = rows.mean().to_frame().T
@@ -113,127 +120,247 @@ def sortEmot(df, t):
 
 
 # ## Импорт файла timeline
-def readTimline(timeData):
-    timeline = pd.read_excel(timeData + 'timeline.xlsx')
+def read_timline(time_data):
+    timeline = pd.read_excel(time_data + 'timeline.xlsx')
     timeline[timeline.columns[2:20]] = timeline[timeline.columns[2:20]].apply(  # парсим время в колонках 2-19
         lambda x: pd.to_datetime(x, format="%M:%S:%f", errors='coerce'))
     return timeline
 
 
 # ## Импорт данных полиграфа
-def readPolyDict(polyChannels, polyData, subject):
-    polyDict = {}
-    readColsP = ['Time', 'Value']
+def read_poly_dict(poly_channels, poly_data, subject):
+    poly_dict = {}
+    read_cols_p = ['Time', 'Value']
 
-    for p in polyChannels:
-        pathP1 = polyData + subject + '\\' + 'Фигуры_' + p + '.txt'  # путь к файлу
-        df = pd.read_table(pathP1, sep='\s+', usecols=readColsP)
+    for p in poly_channels:
+        pathP1 = poly_data + subject + '\\' + 'Фигуры_' + p + '.txt'  # путь к файлу
+        df = pd.read_table(pathP1, sep='\s+', usecols=read_cols_p)
         df['Value'] = df['Value'].replace(',', '.', regex=True).astype(float)
         df['Time'] = df['Time'].apply(
             lambda x: datetime.datetime.utcfromtimestamp(x // 1000).replace(microsecond=x % 1000 * 1000))
-        polyDict[p] = df
-    return polyDict
+        poly_dict[p] = df
+    return poly_dict
 
 
 # ## Импорт FaceReader
-def readFaceReader(faceData, subject):
-    readColsF = ['Video Time', 'Neutral', 'Happy', 'Sad', 'Angry', 'Surprised', 'Scared', 'Disgusted', 'Contempt',
-                 'Valence', 'Arousal']
-    colsF = ['Neutral', 'Happy', 'Sad', 'Angry', 'Surprised', 'Scared', 'Disgusted', 'Contempt', 'Valence', 'Arousal']
+def read_face_reader(face_data, subject):
+    read_cols_f = ['Video Time', 'Neutral', 'Happy', 'Sad', 'Angry', 'Surprised', 'Scared', 'Disgusted', 'Contempt',
+                   'Valence', 'Arousal']
+    cols_f = ['Neutral', 'Happy', 'Sad', 'Angry', 'Surprised', 'Scared', 'Disgusted', 'Contempt', 'Valence', 'Arousal']
     na = ["FIND_FAILED", "FIT_FAILED"]  # значения NA
-    pathF1 = faceData + subject + '.txt'
-    emotions = pd.read_table(pathF1, sep='\t', skiprows=8, index_col=False, na_values=na, usecols=readColsF)
-    emotions[colsF] = emotions[colsF].apply(pd.to_numeric, errors='coerce').fillna(emotions)
+    path_f1 = face_data + subject + '.txt'
+    emotions = pd.read_table(path_f1, sep='\t', skiprows=8, index_col=False, na_values=na, usecols=read_cols_f)
+    emotions[cols_f] = emotions[cols_f].apply(pd.to_numeric, errors='coerce').fillna(emotions)
     emotions['Video Time'] = pd.to_datetime(emotions['Video Time'], errors='coerce')
     return emotions
 
 
+def common_poly_frames(meta_inf, subject):
+    poly_data = meta_inf.get('poly_data')
+    time_data = meta_inf.get('time_data')
+    poly_channels = meta_inf.get('poly_channels')
+    timeline = read_timline(time_data)
+
+    poly_dict = read_poly_dict(poly_channels, poly_data, subject)
+    poly_frames = {}
+    # #### Скелет итогового датафрейма
+    for key in poly_dict:
+        poly_frames[key] = pd.DataFrame(columns=['id', 'name', 'time', 'stimul', 'trial', key])
+        poly_frames[key].drop(poly_frames[key].index, inplace=True)
+
+    poly_dict2 = {}
+    for k in poly_dict:
+        poly_dict2[k] = copy_a_convert(poly_dict[k])
+
+    end_time = total_mil_sec(poly_dict['EDA']['Time'].iloc[-1].time())
+    sub_id = timeline[timeline['name'] == subject]['id'].iloc[0]
+
+    time = 0  # начальное время
+    stim_milli = stim_prep(timeline, sub_id)
+
+    ic()
+    while time < int(end_time):
+        stim = stim_and_time(stim_milli, time)
+        for k in poly_dict2:
+            p_total = sort_poly(poly_dict2[k], time)
+            poly_frames[k].loc[len(poly_frames[k].index)] = [sub_id, subject, time, stim[0], stim[1], p_total]
+
+        time += 50  # временной шаг (может регулироваться)
+    ic()
+    return poly_frames
+
+
 # ## Формирование итогового выходного массива
-def finalDataFrame(metaInf, subject):
-    dataDir = metaInf.get('dataDir')
-    faceData = metaInf.get('faceData')
-    polyData = metaInf.get('polyData')
-    timeData = metaInf.get('timeData')
-    polyChannels = metaInf.get('polyChannels')
+def final_data_frame(meta_inf, subject):
+    data_dir = meta_inf.get('data_dir')
+    face_data = meta_inf.get('face_data')
+    poly_data = meta_inf.get('poly_data')
+    time_data = meta_inf.get('time_data')
+    poly_channels = meta_inf.get('poly_channels')
 
-    timeline = readTimline(timeData)
+    timeline = read_timline(time_data)
 
-    polyDict = readPolyDict(polyChannels, polyData, subject)
-    emotions = readFaceReader(faceData, subject)
+    poly_dict = read_poly_dict(poly_channels, poly_data, subject)
+    emotions = read_face_reader(face_data, subject)
 
     # #### Скелет итогового датафрейма
-    test = pd.DataFrame(columns=['id', 'name', 'time', 'stimul', 'trial'] + list(emotions.columns)[1:] + polyChannels)
+    test = pd.DataFrame(columns=['id', 'name', 'time', 'stimul', 'trial'] + list(emotions.columns)[1:] + poly_channels)
+
     # #### Формирование итогового датафрейма
     # конечное время в миллисекундах, самая ппоследняя временная метка из двух файлов
-    if polyDict['EDA']['Time'].iloc[-1].time() > emotions['Video Time'].iloc[-1].time():
-        end_time = totalMiliSec(polyDict['EDA']['Time'].iloc[-1].time())
+    if poly_dict['EDA']['Time'].iloc[-1].time() > emotions['Video Time'].iloc[-1].time():
+        end_time = total_mil_sec(poly_dict['EDA']['Time'].iloc[-1].time())
     else:
-        end_time = totalMiliSec(emotions['Video Time'].iloc[-1].time())
+        end_time = total_mil_sec(emotions['Video Time'].iloc[-1].time())
 
-    subID = timeline[timeline['name'] == subject]['id'].iloc[0]
+    sub_id = timeline[timeline['name'] == subject]['id'].iloc[0]
 
     df2 = emotions.copy(deep=True)
     df2['Video Time'] = df2['Video Time'].map(
         lambda x: (x.microsecond + x.second * 1000000 + x.minute * 60 * 1000000 +
                    x.hour * 60 * 60 * 1000000) / 1000)
 
-    polyDict2 = {}
-    for k in polyDict:
-        polyDict2[k] = copyAconvert(polyDict[k])
+    poly_dict2 = {}
+    for k in poly_dict:
+        poly_dict2[k] = copy_a_convert(poly_dict[k])
 
     # очистка итогового датафрейма
     test.drop(test.index, inplace=True)
     time = 0  # начальное время
     pTotal = {}
-    stimMilli = stimPrep(timeline, subID)
+    stim_milli = stim_prep(timeline, sub_id)
     ic()
     while time < int(end_time):
-        for k in polyDict2:
-            pTotal[k] = sortPoly(polyDict2[k], time)
+        for k in poly_dict2:
+            pTotal[k] = sort_poly(poly_dict2[k], time)
 
-        eTotal = sortEmot(df2, time)
-        stim = stimAndTime(stimMilli, time)
+        e_total = sort_emot(df2, time)
+        stim = stim_and_time(stim_milli, time)
         time += 50  # временной шаг (может регулироваться)
-        test.loc[len(test.index)] = [subID, subject, time, stim[0], stim[1], eTotal['Neutral'][0], eTotal['Happy'][0],
-                                     eTotal['Sad'][0], eTotal['Angry'][0], eTotal['Surprised'][0],
-                                     eTotal['Scared'][0], eTotal['Disgusted'][0], eTotal['Contempt'][0],
-                                     eTotal['Valence'][0], eTotal['Arousal'][0], pTotal['ABDOMINAL_RESP'],
+        test.loc[len(test.index)] = [sub_id, subject, time, stim[0], stim[1], e_total['Neutral'][0],
+                                     e_total['Happy'][0],
+                                     e_total['Sad'][0], e_total['Angry'][0], e_total['Surprised'][0],
+                                     e_total['Scared'][0], e_total['Disgusted'][0], e_total['Contempt'][0],
+                                     e_total['Valence'][0], e_total['Arousal'][0], pTotal['ABDOMINAL_RESP'],
                                      pTotal['ABS_BLOOD_VOLUME'], pTotal['BLOOD_VOLUME'], pTotal['EDA'],
                                      pTotal['HEART_RATE'], pTotal['PLE'], pTotal['THORACIC_RESP'], pTotal['TONIC_EDA'],
                                      pTotal['TREMOR']]
 
         pTotal.clear()
-        eTotal = eTotal.iloc[0:0]
+        e_total = e_total.iloc[0:0]
 
-    test.to_csv(dataDir + subject + '.csv', encoding='utf-8', index=False)
+    test.to_csv(data_dir + subject + '.csv', encoding='utf-8', index=False)
     ic()
     return test
 
 
-def normalizeFinalDF(polyFrame):
+def normalize_final_df(final_df):
     # удаляем строки, которые не относятся к предъявлениям
-    df = polyFrame[polyFrame['trial'].notnull()]
+    df = final_df[final_df['trial'].notnull()]
     # массив по трем предъявлениям
     # range(5, df.shape[1]) индексы колонок для нормализации
-    for trialNum in 1, 2, 3:
+    for trial_num in 1, 2, 3:
         for col in range(5, df.shape[1]):
-            df.loc[df['trial'] == trialNum, df.columns[col]] = zscore(df.loc[df['trial'] == trialNum, df.columns[col]])
+            df.loc[df['trial'] == trial_num, df.columns[col]] = zscore(
+                df.loc[df['trial'] == trial_num, df.columns[col]])
     return df
 
 
-if __name__ == "__main__":
-    metaInformation = argParser()
-    subjects = metaInformation.get('subjects')
-    # для конкретного объекта по его индексу
-    frame2 = finalDataFrame(metaInformation, subjects[2])
-    frame2Norm = normalizeFinalDF(frame2)
-    # d = pd.read_csv('D:\\dataPoly\\Белоус Полина.csv')
-    # frameNorm = normalizeFinalDF(d)
+def normalize_poly_df(poly_frame):
+    # удаляем строки, которые не относятся к предъявлениям
+    sorted = poly_frame[poly_frame['trial'].notnull()]
+    df = sorted.loc[sorted.iloc[:, -1] != 0]
+    # массив по трем предъявлениям
+    for trial_num in 1, 2, 3:
+        df.loc[df['trial'] == trial_num, df.columns[-1]] = zscore(df.loc[df['trial'] == trial_num, df.columns[-1]])
+    return df
 
+
+def window(start, end, df):
+    start_time = df['time'].iloc[0] + start
+    end_time = df['time'].iloc[0] + start + end
+    sort = df[(df['time'] >= start_time) & (df['time'] <= end_time)]
+    return sort
+
+
+def metrics_EDA(df):
+    groups = df.groupby(["trial", "stimul"])  # выделяем отдельные датафреймы по предъявлению и стимулу
+    result_df = pd.DataFrame(columns=['id', 'name', 'stimul', 'trial', 'EDA'])
+    result_df.drop(result_df.index, inplace=True)
+    id_num = df.iloc[0]['id']
+    name_subj = df.iloc[0]['name']
+    for name, group in groups:
+        win = window(EDA_WINDOW['start'], EDA_WINDOW['end'], group)
+        plt.plot(win['time'], win['EDA'])
+        plt.show()
+        max_index = win['EDA'].idxmax()
+        sliced = win.loc[:max_index, :]
+        reverse = sliced.iloc[::-1]
+        min_index = reverse['EDA'].idxmin()
+        jump_value = win.loc[max_index, 'EDA'] - win.loc[min_index, 'EDA']
+        new_row = {'id': id_num, 'name': name_subj, 'stimul': name[1], 'trial': name[0], 'EDA': jump_value}
+        result_df.loc[len(result_df.index)] = new_row
+    return result_df
+
+
+def metrics_resp(dataframe):
+    groups = dataframe.groupby(["trial", "stimul"])  # выделяем отдельные датафреймы по предъявлению и стимулу
+    result_df = pd.DataFrame(columns=['id', 'name', 'stimul', 'trial', 'THORACIC_RESP'])
+    result_df.drop(result_df.index, inplace=True)
+    id_num = dataframe.iloc[0]['id']
+    name_subj = dataframe.iloc[0]['name']
+    for name, group in groups:
+        df = window(BREATHE_WINDOW['start'], BREATHE_WINDOW['end'], group)
+        plt.plot(df['time'], df['THORACIC_RESP'])
+        plt.show()
+        # вычисляем разности между соседними значениями x и y
+        df['dx'] = df['time'].diff()
+        df['dy'] = df['THORACIC_RESP'].diff()
+        # возводим каждую разность в квадрат
+        df['dx^2'] = df['dx'] ** 2
+        df['dy^2'] = df['dy'] ** 2
+        # суммируем все полученные значения
+        sum_dx2_dy2 = df['dx^2'].sum() + df['dy^2'].sum()
+        # извлекаем квадратный корень из суммы
+        length = sum_dx2_dy2 ** 0.5
+        new_row = {'id': id_num, 'name': name_subj, 'stimul': name[1], 'trial': name[0], 'THORACIC_RESP': length}
+        result_df.loc[len(result_df.index)] = new_row
+
+    return result_df
+
+
+if __name__ == "__main__":
+    meta_information = arg_parser()
+    subjects = meta_information.get('subjects')
+    # для конкретного объекта по его индексу
+
+    # frame2 = finalDataFrame(meta_information, subjects[2])
+    # frame2Norm = normalizeFinalDF(frame2)
+    # t = common_poly_frames(meta_information, subjects[2])
+    # norm = normalize_poly_df(t['EDA'])
+    # plt1 = plt.plot(norm['time'], norm['EDA'])
+    # win = window(EDA_WINDOW['start'], EDA_WINDOW['end'], norm)
+    # plt2 = plt.plot(win['time'], win['EDA'])
+    # val = metrics_EDA(win)
+
+    # метрика для кгр
+    norm_EDA = normalize_poly_df(common_poly_frames(meta_information, subjects[1])['EDA'])
+    val_EDA = metrics_EDA(norm_EDA)
+    print(val_EDA)
+
+    # метрика для дыхания
+    norm_RESP = normalize_poly_df(common_poly_frames(meta_information, subjects[1])['THORACIC_RESP'])
+    val_resp = metrics_resp(norm_RESP)
+    print(val_resp)
     # для нескольких объектов по их индексам
-    # for i in 0, 1:
-    #     finalDataFrame(metaInformation, subjects[i])
+    # for i in 0, 1, 2:
+    #     norm_EDA = normalize_poly_df(common_poly_frames(meta_information, subjects[i])['EDA'])
+    #     val_EDA = metrics_EDA(norm_EDA)
+    #     arr.append(val_EDA)
+    #     norm_RESP = normalize_poly_df(common_poly_frames(meta_information, subjects[i])['THORACIC_RESP'])
+    #     val_resp = metrics_resp(norm_RESP)
+    #     arr2.append(val_resp)
 
     # для всех участников эксперимента
     # for subj in subjects:
-    #     finalDataFrame(metaInformation, subj)
+    #     finalDataFrame(meta_information, subj)
